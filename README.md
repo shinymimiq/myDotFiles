bf86f489c6f3f703bb3cb4a1cf9722ae39a85711
Tony Liu <mrtoniliu@gmail.com>
2021-01-16 13:34:49 +0800

    Update

--- README.md
+++ README.md
@@ -2,10 +2,5 @@
 
 Currently contain:
 
-- i3 config
-- i3 lockscreen shell script
-- i3 status bar
-- tmux config (Not used, use i3 instead)
-- vimrc
 - zshrc
-- ycm_extra_conf.py (Not used)
+- tmux.conf
--- i3/i3lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 10x10 /tmp/screen_locked2.png
-# mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-i3lock -i /tmp/screen_locked2.png
-# Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
